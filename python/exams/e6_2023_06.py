d13750143c23d5b78030fff1840d946a1a331ebd
NikodemMlynski <104516104+NikodemMlynski@users.noreply.github.com>
2025-11-03 19:14:31 +0100

    Update e6_2023_06.py

--- python/exams/e6_2023_06.py
+++ python/exams/e6_2023_06.py
@@ -1,31 +1,27 @@
-def generate_array(n):
-    arr = [] 
-    for i in range(1, n+2):
+
+arr = []
+print(i)
+
+n = 100
+
+def fill_array(arr, n):
+    for i in range(n + 1):
         arr.append(True)
 
-    return arr 
-    
 
-def get_first_numbers(n):
-    A = generate_array(n)
-    for i in range(2, n+1):
-        if A[i] == True:
-            for j in range(2*i, n+1, i):
-                A[j] = False
-    return A 
+def eratostenes(arr, n):
+    for i in range(2, int(n**0.5) + 1):
+        if arr[i] == True:
+            for j in range(2 * i, n + 1, i):
+                arr[j] = False 
 
+fill_array(arr, n)
 
-def fill_first_numbers_array(bool_arr):
-    arr = []
-    for idx, val in enumerate(bool_arr):
-        if val == True:
-            arr.append(str(idx))
-    return arr 
+eratostenes(arr, n)
 
-def display_first_numbers(arr):
-    print("Liczby pierwsze z przedziału 1 - 100")
-    print(", ".join(arr))
+def display_first_numbers(arr, n):
+    for i in range(2, n + 1):
+        if arr[i] == True:
+            print(i)
 
-bool_arr = get_first_numbers(101)
-first_numbers = fill_first_numbers_array(bool_arr)
-display_first_numbers(first_numbers)
+display_first_numbers(arr, n)
